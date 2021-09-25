065fcbf0f4780ea2fc7b941102ec1381517fa74e
André Kanayama <andre.spanguero@gmail.com>
2021-09-25 16:50:22 -0300

    Add first project resources

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
+
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+
 const deleteAccount = async event => {
-  console.log(event)
+  const sns = new AWS.SNS()
+
+  const deleteAccountEvent = {
+    ...event,
+    timestamp: Math.floor(new Date().getTime() / 1000),
+  }
+
+  console.log(deleteAccountEvent)
+
+  return sns.publish({
+    Message: JSON.stringify(deleteAccountEvent),
+    TopicArn: process.env.CF_DeleteAccountTopic,
+  }).promise()
 }
 
 module.exports = {
