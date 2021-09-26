cbecefabf288d56ab9de450b8c2d32112be5ecb3
André <andre.spanguero@gmail.com>
2021-09-26 17:25:39 -0300

    Update consumer.js

--- src/consumer.js
+++ src/consumer.js
@@ -2,20 +2,22 @@ const AWSXRay = require('aws-xray-sdk-core')
 const AWS = AWSXRay.captureAWS(require('aws-sdk'))
 const getUuid = require('uuid-by-string')
 
-var documentClient = new AWS.DynamoDB.DocumentClient()
+const documentClient = new AWS.DynamoDB.DocumentClient()
 
-const recordDeleteAccountInfo = event => {
+const recordDeletedAccountInfo = event => {
   const { email, timestamp } = JSON.parse(event.Records[0].body)
+
   const params = {
-    TableName: process.env.DYNAMODB_TABLE_NAME,
+    TableName: process.env.DYNAMODB_TABLE,
     Item: {
       userId: getUuid(email, 'rocket'),
       deletedAt: timestamp,
     },
   }
+
   return documentClient.put(params).promise()
 }
 
 module.exports = {
-  recordDeleteAccountInfo,
+  recordDeletedAccountInfo,
 }
